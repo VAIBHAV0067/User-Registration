11050fad8153e6da2be3ab253fbd10fe7670e1aa
tutorial6 <manapure.vaibhav542@gmail.com>
2022-03-28 19:37:19 +0530

    Validate First Name

---
+++ Registration/Registration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ Registration/Registration/Validation_regex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Validation_regex
+    {
+        public void Checkfirstname(string firstname)
+        {
+            string Firstnamepattern = "^[A-Z][a-z]{3,10}$";
+            if (Regex.IsMatch(firstname, Firstnamepattern))
+                Console.WriteLine("First Name is valid");
+            else
+                Console.WriteLine("Invalid name!!");
+        }
+    }
+}
+
