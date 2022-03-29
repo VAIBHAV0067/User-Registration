106eab85a22f833f7365f56734c35c0523da221f
tutorial6 <manapure.vaibhav542@gmail.com>
2022-03-29 19:07:48 +0530

    Valid Last Name

--- Registration/Registration/Program.cs
+++ Registration/Registration/Program.cs
@@ -12,6 +12,31 @@ namespace UserRegistration
             {
                 Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("*First letter should be capital.");
+                        Console.WriteLine("*length should be Minimum three characters.");
+                        Console.WriteLine("Enter First name :");
+                        string fname = Console.ReadLine();
+                        Validation_regex Checkpattern = new Validation_regex();
+                        Checkpattern.Checkfirstname(fname);
+                        break;
+                    case 2:
+                        Console.WriteLine("*First letter should be capital.");
+                        Console.WriteLine("*length should be Minimum three characters.");
+                        Console.WriteLine("Enter Last name :");
+                        string lname = Console.ReadLine();
+                        Validation_regex lnamepattern = new Validation_regex();
+                        lnamepattern.Checklastname(lname);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("invalid option");
+                        break;
+                }
             }
         }
     }
